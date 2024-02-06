d27260f03bff01b3dcff75f7d88014fc4af0fcf4
Пол Касуса <65332516+can7wysiwyg@users.noreply.github.com>
2024-02-06 17:09:30 +0200

    progress

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const withTooltipItems = document.querySelectorAll('.with-tooltip');
+
+    withTooltipItems.forEach(item => {
+        const tooltipId = item.getAttribute('data-tooltip-content');
+        const tooltipContent = document.getElementById(tooltipId);
+        
+        // Adding event listeners to both the trigger item and the tooltip content
+        item.addEventListener('mouseover', () => showTooltipContent(tooltipContent));
+        item.addEventListener('mouseout', () => hideTooltipContent(tooltipContent));
+        tooltipContent.addEventListener('mouseover', () => showTooltipContent(tooltipContent));
+        tooltipContent.addEventListener('mouseout', () => hideTooltipContent(tooltipContent));
+    });
+
+    function showTooltipContent(content) {
+        if (content) {
+            content.style.display = 'flex';
+        }
+    }
+
+    function hideTooltipContent(content) {
+        if (content) {
+            content.style.display = 'none';
+        }
+    }
+
+    // Show content for the hovered item in the tooltip
+    const tooltipItems = document.querySelectorAll('.tooltip-item');
+    const tooltipContentItems = document.querySelectorAll('.tooltip-content-item');
+
+    tooltipItems.forEach((item, index) => {
+        item.addEventListener('mouseover', () => {
+            // Hide all content items
+            tooltipContentItems.forEach(contentItem => {
+                contentItem.style.display = 'none';
+            });
+            // Show content item corresponding to the hovered item
+            tooltipContentItems[index].style.display = 'block';
+        });
+    });
+});
+
+
